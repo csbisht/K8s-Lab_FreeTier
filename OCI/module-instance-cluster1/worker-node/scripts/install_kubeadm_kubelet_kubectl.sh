cb6d290ebe76d92424643037dcfb4ae9ce1f56c2
Bisht <chandan.o.singh@capgemini.com>
2021-12-19 21:29:22 +0530

    update kubeadm install script

--- OCI/module-instance-cluster1/worker-node/scripts/install_kubeadm_kubelet_kubectl.sh
+++ OCI/module-instance-cluster1/worker-node/scripts/install_kubeadm_kubelet_kubectl.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-sudo apt-get update
-sleep 10
-sudo apt-get install -y kubelet=1.19.0-00 kubeadm=1.19.0-00 kubectl=1.19.0-00
+for i in {1..3}
+do
+findkubeadm=`command -v kubeadm`
+findkubeadmtest="$?"
 
+if [ "${findkubeadmtest}" -gt 0 ]; then
+sudo apt-get update 
+sleep 5
+sudo apt-get install -y kubelet=1.19.0-00 kubeadm=1.19.0-00 kubectl=1.19.0-00
 sudo apt-mark hold kubelet kubeadm kubectl
+sleep 5
+else 
+echo -e "kubeadm installed. \n"
+exit 0
+fi
+done
