a3a59516824788cf9dbc2a12a868901442efd164
Bisht <chandan.o.singh@capgemini.com>
2021-12-26 21:05:27 +0530

    update install_kubeadm_kubelet_kubectl script

--- OCI/module-instance-cluster1/master/scripts/install_kubeadm_kubelet_kubectl.sh
+++ OCI/module-instance-cluster1/master/scripts/install_kubeadm_kubelet_kubectl.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
+kubeletversion="1.19.0-00"
+kubeadmversion="1.19.0-00"
+kubectlversion="1.19.0-00"
+
 for i in {1..3}
 do
 findkubeadm=`command -v kubeadm`
 findkubeadmtest="$?"
 
 if [ "${findkubeadmtest}" -gt 0 ]; then
-sudo apt-get update 
+sudo apt-get update
 sleep 5
-sudo apt-get install -y kubelet=1.19.0-00 kubeadm=1.19.0-00 kubectl=1.19.0-00
+sudo apt-get install -y kubelet="${kubeletversion}" kubeadm="${kubeadmversion}" kubectl="${kubectlversion}"
 sudo apt-mark hold kubelet kubeadm kubectl
 sleep 5
-else 
+else
 echo -e "kubeadm installed. \n"
+
+####find and install kubelet if not installed
+findkubelet=`command -v kubelet`
+findkubelettest="$?"
+if [ "${findkubelettest}" -gt 0 ]; then
+sudo apt-get update
+sleep 5
+sudo apt-get install -y kubelet="${kubeletversion}"
+sudo apt-mark hold kubelet kubeadm kubectl
+sleep 5
+else
+echo -e "kubelet installed. \n"
+
+####find and install kubectl if not installed
+findkubectl=`command -v kubectl`
+findkubectltest="$?"
+if [ "${findkubectltest}" -gt 0 ]; then
+sudo apt-get update
+sleep 5
+sudo apt-get install -y kubectl="${kubectlversion}"
+sudo apt-mark hold kubelet kubeadm kubectl
+sleep 5
+else
+echo -e "kubectl installed. \n"
+exit 0
+fi
+fi
+
 exit 0
 fi
 done
